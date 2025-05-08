54c2d2b7a472cf26d7a91f7acdf8fa71deac12e9
Logan Bussell <loganbussell@microsoft.com>
2025-05-08 14:51:55 -0700

    Add cli boilerplate

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ConsoleAppFramework;
+
+var app = ConsoleApp.Create();
+app.Add<Commands>();
+app.Run(args);
+
+class Commands
+{
+    public void Commits(string to)
+    {
+        Console.WriteLine("List of commits to {0}", to);
+    }
+}
